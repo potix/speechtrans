afbf7e1ee1bda268d1858b5c9b2c5b27872a2e1b
poti <poti.dog@gmail.com>
2022-06-02 14:34:01 +0000

    update

--- resource/js/recorder_worklet.js
+++ resource/js/recorder_worklet.js
@@ -13,6 +13,23 @@ class RecorderWorkletProcessor extends AudioWorkletProcessor {
 			newInputs.push(newChannels)
 
 		}
+
+		/* loopbeck */
+		/*
+		for (const idx1 in inputs) {
+			let inChannels = inputs[idx1] 
+			let outChannels = outputs[idx1]
+			for (const idx2 in inChannels) {
+				let inChannel = inChannels[idx2]
+				let outChannel = outChannels[idx2]
+				for (const idx3 in inChannel) {
+					outChannel[idx3] = inChannel[idx3]
+				}
+
+			}
+		}
+		*/
+
 		let message = { MType: "inAudio", RawData: newInputs };
 		let jsonMessage = JSON.stringify(message);
 		this.port.postMessage(jsonMessage);
